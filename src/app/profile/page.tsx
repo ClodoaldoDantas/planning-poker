f629550b635959ce930c839d33a65fb4b2f7e29d
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-05 16:50:36 -0300

    feat: página de perfil do usuário

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,10 @@
 import { Center } from '@/components/center'
-import { Header } from '@/components/header'
 import { Profile } from '@/components/profile'
 import { BackButton } from '@/components/back-button'
 
 import styles from './page.module.scss'
+import Link from 'next/link'
+import { ChevronRight, Heart, LogOut, Tv2 } from 'lucide-react'
 
 export default function ProfilePage() {
   return (
@@ -11,11 +12,33 @@ export default function ProfilePage() {
       <section className={styles.profilePage}>
         <BackButton href="/lobby">Voltar para o lobby</BackButton>
 
-        <Header>
-          <Profile />
-        </Header>
+        <header className={styles.profilePageHeader}>
+          <div className={styles.profilePageHeaderContent}>
+            <h2>Taylor Owens</h2>
+            <p>taylor@example.com</p>
+          </div>
 
-        <div>Tela de perfil</div>
+          <Profile />
+        </header>
+
+        <nav className={styles.profilePageMenu}>
+          <Link href="/profile">
+            <Tv2 size={20} />
+            <span>Acessar meus canais</span>
+            <ChevronRight className={styles.arrow} size={20} />
+          </Link>
+
+          <Link href="/profile">
+            <Heart size={20} />
+            <span>Acessar favoritos</span>
+            <ChevronRight className={styles.arrow} size={20} />
+          </Link>
+
+          <button type="button">
+            <LogOut size={20} />
+            Sair da Conta
+          </button>
+        </nav>
       </section>
     </Center>
   )
