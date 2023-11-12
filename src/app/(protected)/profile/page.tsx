427cb4ad4fcf900a0ccc1757c0aa9b2787225b05
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-12 20:59:56 -0300

    feat: alterando rotas do menu na tela de perfil

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { ChevronRight, Heart, Tv2 } from 'lucide-react'
+import { ChevronRight, Heart, Home, Tv2 } from 'lucide-react'
 import { Center } from '@/components/center'
 import { BackButton } from '@/components/back-button'
 import { SignOutButton } from '@/components/sign-out-button'
@@ -17,14 +17,20 @@ export default function ProfilePage() {
 
         <nav className={styles.profilePageMenu}>
           <Link href="/lobby">
-            <Tv2 size={20} />
+            <Home size={20} />
             <span>Ir para o lobby</span>
             <ChevronRight className={styles.arrow} size={20} />
           </Link>
 
           <Link href="/profile">
+            <Tv2 size={20} />
+            <span>Acessar minhas salas</span>
+            <ChevronRight className={styles.arrow} size={20} />
+          </Link>
+
+          <Link href="/profile">
             <Heart size={20} />
-            <span>Acessar favoritos</span>
+            <span>Acessar salas favoritas</span>
             <ChevronRight className={styles.arrow} size={20} />
           </Link>
 
