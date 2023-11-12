c4b9f6b906fb782a9eaef30eb542e02f4b090a57
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-12 15:49:32 -0300

    feat: criando interface dos cards

--- src/app/(protected)/rooms/[id]/planning/task-item/index.tsx
+++ src/app/(protected)/rooms/[id]/planning/task-item/index.tsx
@@ -2,8 +2,22 @@ import { ClipboardCheck } from 'lucide-react'
 import { Task } from '@/types/task'
 
 import styles from './styles.module.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
+
+const tShirts = ['XS', 'S', 'M', 'L', 'XL', 'XXL', '?', '☕']
 
 export function TaskItem({ task }: { task: Task }) {
+  const [activeCard, setActiveCard] = useState<string | null>(null)
+
+  function handleSelectCard(card: string) {
+    if (card === activeCard) {
+      setActiveCard(null)
+    } else {
+      setActiveCard(card)
+    }
+  }
+
   return (
     <section key={task.id} className={styles.card}>
       <header className={styles.cardHeader}>
@@ -15,6 +29,20 @@ export function TaskItem({ task }: { task: Task }) {
 
       <div className={styles.cardBody}>
         <p>{task.description}</p>
+
+        <ul className={styles.cardList}>
+          {tShirts.map((item) => (
+            <li className={styles.cardListItem} key={item}>
+              <button
+                type="button"
+                onClick={() => handleSelectCard(item)}
+                className={classNames({ [styles.active]: activeCard === item })}
+              >
+                {item}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   )
