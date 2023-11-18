cb7df04fb0e69a0358bcddad2a0df60a04b94544
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-11-17 21:15:52 -0300

    feat: adicionando e removendo votos

--- src/app/(protected)/rooms/[id]/planning/task-item/index.tsx
+++ src/app/(protected)/rooms/[id]/planning/task-item/index.tsx
@@ -3,18 +3,65 @@ import { useState } from 'react'
 import { ClipboardCheck } from 'lucide-react'
 import { Task } from '@/types/task'
 
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore'
+import { db } from '@/lib/firebase'
+import { useAuth } from '@/contexts/AuthContext'
+
 import styles from './styles.module.scss'
 
 const tShirts = ['XS', 'S', 'M', 'L', 'XL', 'XXL', '?', '☕']
 
 export function TaskItem({ task }: { task: Task }) {
   const [activeCard, setActiveCard] = useState<string | null>(null)
+  const { user } = useAuth()
+
+  async function addVote(card: string) {
+    try {
+      const taskRef = doc(db, 'tasks', task.id)
+      const votesSubCollection = collection(taskRef, 'votes')
+
+      addDoc(votesSubCollection, {
+        value: card,
+        userId: user?.id,
+        userName: user?.name,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function deleteVote() {
+    try {
+      const taskRef = doc(db, 'tasks', task.id)
+      const votesSubCollection = collection(taskRef, 'votes')
+
+      const q = query(votesSubCollection, where('userId', '==', user?.id))
+      const querySnapshot = await getDocs(q)
+
+      const vote = querySnapshot.docs[0]
+      if (!vote.exists) return
+
+      await deleteDoc(doc(votesSubCollection, vote.id))
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   async function handleSelectCard(card: string) {
     if (card === activeCard) {
       setActiveCard(null)
+      await deleteVote()
     } else {
       setActiveCard(card)
+      await addVote(card)
     }
   }
 
